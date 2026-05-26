a1fbbf905a77008a6ea756d2700c0a045e8c36bc
“SebastianGarb” <sebastiangarboza63@hotmail.com>
2026-05-25 19:10:20 -0500

    version2-solucion1

--- solucion1.py
+++ solucion1.py
@@ -12,14 +12,14 @@ def min_max_m3(lst, x=0):
         else:
             return min_r, max_r
 
-n = int(input("Ingrese el tamaño del arreglo: "))
+n = int(input("Ingrese el tamaño de la lista: "))
 arreglo = [random.randint(10, 9999) for _ in range(n)]
 print(f"Arreglo: {arreglo}")
 
 minimo, maximo = min_max_m3(arreglo)
 
 if minimo == float('inf'):
-    print("No hay múltiplos de 3 en el arreglo.")
+    print("No hay múltiplos de 3.")
 else:
     promedio = (minimo + maximo) / 2
     print(f"Resultado = ({maximo}+{minimo})/2 = {promedio}")
